55210ad1552ea33ee3051da77683153fd15ab145
Mamadou Diagne <genova@dofbi.com>
2018-11-29 17:57:46 +0300

    Component Calculatrice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatriceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculatrice/calculatrice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice',
+  templateUrl: './calculatrice.component.html',
+  styleUrls: ['./calculatrice.component.css']
+})
+export class CalculatriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
